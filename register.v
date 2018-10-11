ff6521a16c4f9586bcd25e72ca29ff26a777c34f
nielsenlouise <louiseenielsen@gmail.com>
2018-10-10 21:35:47 -0400

    figured out a bit more what i was doing

--- register.v
+++ register.v
@@ -26,16 +26,8 @@ input [31:0]      d,
 input             wrenable,
 input             clk
 );
-  // // Generate a bunch of registers and assign them
-  // genvar i;
-  // generate
-  //   for (i=0; i<32; i=i+1)
-  //   begin:genblock
-  //       register reg1bit(q[i], d[i], wrenable, clk);
-  //   end
-  // endgenerate
 
-  // Alternately... I can just do this...
+  // Just like the 1-bit register
   always @(posedge clk) begin
     if (wrenable) begin
       q <= d;
@@ -47,23 +39,12 @@ endmodule
 // Ignores inputs, just outputs 32b'0
 module register32zero
 (
-output reg [31:0] q,
-// output [31:0]     q,
+output [31:0]     q,
 input [31:0]      d,
 input             wrenable,
 input             clk
 );
-  // // Regardless of input, q is zero
-  // assign q = 0;
+  // Regardless of input, q is zero
+  assign q = 0;
 
-  // or ... with output reg ...
-  // would this just cycle forever?
-  always @(1) begin
-    q <= 0;
-  end
-
-  // // in case the above cycles forever and breaks by not compiling or something
-  // always @(posedge clk) begin
-  //   q <= 0;
-  // end
 endmodule
